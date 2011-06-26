18b045585a4a8efa1bb9d8d869e04036014c4448
Leandro Silva <leandrodoze@gmail.com>
2011-06-26 18:58:45 -0300

    refactored to use clojure.contrib.json/json-str (done)

--- src/qdis/web/handler.clj
+++ src/qdis/web/handler.clj
@@ -26,22 +26,22 @@
       {:status 200
        :headers {"Content-Type" "application/json"
                  "Location" (str "/queue/" queue "/" item-uuid "/status")}
-       :body (str "{\"queue\":\"" queue "\","
-                  " \"item-uuid\":\"" item-uuid "\"}")}))
+       :body (json-str {"queue" queue
+                        "item-uuid" item-uuid})}))
 
   (GET "/queue/:queue/dequeue" [queue]
     (let [result (qdis.engine.queue/dequeue queue)]
       (if (= result :queue-not-found-or-is-empty)
         {:status 404
          :headers {"Content-Type" "application/json"}
-         :body (str "{\"queue\":\"" queue "\","
-                    " \"message\":\"Queue not found or is empty\"}")}
+         :body (json-str {"queue" queue
+                          "message" "Queue not found or is empty"})}
          
         {:status 200
          :headers {"Content-Type" "application/json"}
-         :body (str "{\"queue\":\"" queue "\","
-                    " \"item-uuid\":\"" (:item-uuid result) "\","
-                    " \"item\":" (:item result) "}")})))
+         :body (json-str {"queue" queue
+                          "item-uuid" (:item-uuid result)
+                          "item" (:item result)})})))
 
   (GET "/:queue/:item-uuid/status" [queue item-uuid]
     "Not implemented yet")
