ce4c71449dfd9a8c5dbaf91d101a8b655c6843c9
Leandro Silva <leandrodoze@gmail.com>
2011-06-28 17:59:16 -0300

    refactored

--- src/qdis/web/server.clj
+++ src/qdis/web/server.clj
@@ -2,5 +2,5 @@
   (:use ring.adapter.jetty)
   (:use qdis.web.handler))
 
-(defn start [config]
-  (run-jetty #'qdis.web.handler/app (:server config)))
+(defn start [config-info]
+  (run-jetty #'qdis.web.handler/app (:server config-info)))
