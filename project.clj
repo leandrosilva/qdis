550c85b374035c1f0bf23c9892857873a1eb7737
Leandro Silva <leandrodoze@gmail.com>
2012-06-02 15:22:05 -0300

    added dev-dependencies

--- project.clj
+++ project.clj
@@ -6,8 +6,9 @@
      [commons-pool/commons-pool "1.6"]
      [redis.clients/jedis "2.1.0"]
      [ring/ring-jetty-adapter "1.1.0"]
-     [ring/ring-devel "1.1.0"]
      [compojure "1.1.0"]]
+  :dev-dependencies
+    [[ring/ring-devel "1.1.0"]]
   :plugins
     [[lein-ring "0.7.1"]]
   :ring {:handler qdis.web/app})
